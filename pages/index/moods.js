cc5840e05aa41639871987622843567b5c58214b
Angus Zhu <angus.is.online@gmail.com>
2017-07-16 13:41:38 +0800

    Update

---
+++ pages/index/moods.js
@@ -0,0 +1,117 @@
+const moods = [{
+  id: 0,
+  name: 'ecstasy',
+  color: '#E94B35',
+  param: {
+    lipTopY: 0.558,
+    lipTopCp1X: 0.32,
+    lipLeftX: 0.229,
+    lipLeftY: 0.559,
+    lipLeftCp1X: 0.209,
+    lipLeftCp1Y: 0.711,
+    lipLeftCp2X: 0.351,
+    lipLeftCp2Y: 0.559,
+    lipBottomY: 0.836,
+    lipBottomCp1X: 0.288
+  }
+}, {
+  id: 1,
+  name: 'happy',
+  color: '#F59D00',
+  param: {
+    lipTopY: 0.71,
+    lipTopCp1X: 0.385,
+    lipLeftX: 0.284,
+    lipLeftY: 0.584,
+    lipLeftCp1X: 0.3,
+    lipLeftCp1Y: 0.634,
+    lipLeftCp2X: 0.3,
+    lipLeftCp2Y: 0.634,
+    lipBottomY: 0.71,
+    lipBottomCp1X: 0.385
+  }
+}, {
+  id: 2,
+  name: 'pleased',
+  color: '#1FCE6D',
+  param: {
+    lipTopY: 0.626,
+    lipTopCp1X: 0.385,
+    lipLeftX: 0.29,
+    lipLeftY: 0.6,
+    lipLeftCp1X: 0.37,
+    lipLeftCp1Y: 0.7,
+    lipLeftCp2X: 0.37,
+    lipLeftCp2Y: 0.7,
+    lipBottomY: 0.626,
+    lipBottomCp1X: 0.385
+  }
+}, {
+  id: 3,
+  name: 'calm',
+  color: '#2C97DE',
+  param: {
+    lipTopY: 0.58,
+    lipTopCp1X: 0.385,
+    lipLeftX: 0.379,
+    lipLeftY: 0.58,
+    lipLeftCp1X: 0.397,
+    lipLeftCp1Y: 0.58,
+    lipLeftCp2X: 0.436,
+    lipLeftCp2Y: 0.58,
+    lipBottomY: 0.58,
+    lipBottomCp1X: 0.385
+  }
+}, {
+  id: 4,
+  name: 'upset',
+  color: '#8F3FAF',
+  param: {
+    lipTopY: 0.5,
+    lipTopCp1X: 0.415,
+    lipLeftX: 0.35,
+    lipLeftY: 0.65,
+    lipLeftCp1X: 0.35,
+    lipLeftCp1Y: 0.565,
+    lipLeftCp2X: 0.35,
+    lipLeftCp2Y: 0.565,
+    lipBottomY: 0.5,
+    lipBottomCp1X: 0.415
+  }
+}, {
+  id: 5,
+  name: 'oops',
+  color: '#7F8C8D',
+  param: {
+    lipTopY: 0.5,
+    lipTopCp1X: 0.373,
+    lipLeftX: 0.374,
+    lipLeftY: 0.65,
+    lipLeftCp1X: 0.391,
+    lipLeftCp1Y: 0.786,
+    lipLeftCp2X: 0.364,
+    lipLeftCp2Y: 0.585,
+    lipBottomY: 0.75,
+    lipBottomCp1X: 0.415
+  }
+}, {
+  id: 6,
+  name: 'ugh',
+  color: '#2B3E51',
+  param: {
+    lipTopY: 0.617,
+    lipTopCp1X: 0.373,
+    lipLeftX: 0.264,
+    lipLeftY: 0.637,
+    lipLeftCp1X: 0.268,
+    lipLeftCp1Y: 0.864,
+    lipLeftCp2X: 0.265,
+    lipLeftCp2Y: 0.546,
+    lipBottomY: 0.714,
+    lipBottomCp1X: 0.415
+  }
+}]
+
+module.exports = {
+  moods: moods
+}
