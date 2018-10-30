de8085272ce94ad04821c78769ad71e3157dc63a
angusjune <angus.is.online@gmail.com>
2018-10-30 14:06:29 +0800

    Change environment to production

--- client/config.js
+++ client/config.js
@@ -1,5 +1,6 @@
 const cdnHost = 'http://cdn.popppular.com'
-const cloudEnv = 'te-ba2bd8'
+// const cloudEnv = 'te-ba2bd8'
+const cloudEnv = 'feeltoday-3ea7bd'
 const dbCollection = 'feeltoday'
 
 let config = {
